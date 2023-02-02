fb89cf47b04ebe9806f105b0e61ab249836083ff
Gulsah <gulsahyigit3@gmail.com>
2023-02-02 16:16:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0 });
+
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
+
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
+});
